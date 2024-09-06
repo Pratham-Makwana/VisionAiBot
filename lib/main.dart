ff84a1c436ddd85dd944c2daad5bcebb2f0b9e4a
Pratham-Makwana <makwanapratham13@gmail.com>
2024-09-06 13:47:54 +0530

    Animation OnBoarding Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
 import 'package:ai_assistant/screen/splash__screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:hive/hive.dart';
+import 'package:path_provider/path_provider.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  /// for initializing hive to use app directory
+  /// In Hive Everything is box and we can store any data in box
+  /// we have to create and open a box  to store data in hive
+  Hive.defaultDirectory = (await getApplicationDocumentsDirectory()).path;
   await SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
   await SystemChrome.setPreferredOrientations(
       [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
@@ -16,8 +23,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home:SplashScreen()
-    );
+        debugShowCheckedModeBanner: false, home: SplashScreen());
   }
 }
--- lib/screen/onboarding_screen.dart
+++ lib/screen/onboarding_screen.dart
@@ -1,3 +1,5 @@
+import 'package:ai_assistant/model/onboard.dart';
+import 'package:ai_assistant/screen/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
 
@@ -8,71 +10,111 @@ class OnboardingScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+
+    final controller = PageController();
+    final list = [
+      /// OnBoarding 1
+      OnBoard(
+          title: 'Ask me Anything',
+          subtitle:
+              'I can be your Best Friend & You can ask me anything & I will help you!',
+          lottie: 'ai_ask_me'),
+      OnBoard(
+          title: 'Imagination To Reality',
+          subtitle:
+              'Just Imagine anything & let me know , I will create something wonderful for you',
+          lottie: 'ai_play')
+    ];
     return Scaffold(
-      body: Column(
-        children: [
-          /// lottie
-          Lottie.asset('assets/lottie/ai_ask_me.json', height: mq.height * .6),
+        body: PageView.builder(
+      controller: controller,
+      itemCount: list.length,
+      itemBuilder: (context, index) {
+        final isLast = index == list.length - 1;
+        return Column(
+          children: [
+            /// lottie
+            Lottie.asset('assets/lottie/${list[index].lottie}.json',
+                height: mq.height * .6, width: isLast ? mq.width * .8 : null),
 
-          /// title
-          const Text(
-            'Ask me Anything',
-            style: TextStyle(
-                fontSize: 18, fontWeight: FontWeight.w900, letterSpacing: 0.5),
-          ),
+            /// title
+            Text(
+              list[index].title,
+              style: const TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w900,
+                  letterSpacing: 0.5),
+            ),
 
-          /// for adding some space
-          SizedBox(
-            height: mq.height * .01,
-          ),
+            /// for adding some space
+            SizedBox(
+              height: mq.height * .01,
+            ),
 
-          /// subtitle
-          SizedBox(
-              width: mq.width * .7,
-              child: const Text(
-                'I can be your Best Friend & You can ask me anything & I will help you!',
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 13.5, letterSpacing: 0.5),
-              )),
+            /// subtitle
+            SizedBox(
+                width: mq.width * .7,
+                child: Text(
+                  list[index].subtitle,
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(fontSize: 13.5, letterSpacing: 0.5),
+                )),
 
-          /// for adding some space
-          const Spacer(),
-          /// dots
-          Wrap(
-            spacing: 10,
-            children: List.generate(
-                2,
-                (i) => Container(
-                      width: 10,
-                      height: 8,
-                      decoration: const BoxDecoration(
-                        color: Colors.grey,
-                        borderRadius: BorderRadius.all(Radius.circular(5)),
-                      ),
-                    )),
-          ),
-          /// for adding some space
-          const Spacer(),
+            /// for adding some space
+            const Spacer(),
 
-          /// Button
-          ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                  minimumSize: Size(mq.width * .4, 50),
-                  backgroundColor: Colors.blue,
-                  shape: const StadiumBorder(),
-                  elevation: 0),
-              onPressed: () {},
-              child: const Text(
-                'Next',
-                style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 17,
-                    fontWeight: FontWeight.w500),
-              )),
-          /// for adding some space
-          const Spacer(flex: 2,),
-        ],
-      ),
-    );
+            /// dots
+            Wrap(
+              spacing: 10,
+              children: List.generate(
+                  list.length,
+                  (i) => Container(
+                        width: i == index ? 15 : 10,
+                        height: 8,
+                        decoration: BoxDecoration(
+                          color: i == index ? Colors.blue : Colors.grey,
+                          borderRadius:
+                              const BorderRadius.all(Radius.circular(5)),
+                        ),
+                      )),
+            ),
+
+            /// for adding some space
+            const Spacer(),
+
+            /// Button
+            ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    minimumSize: Size(mq.width * .4, 50),
+                    backgroundColor: Colors.blue,
+                    shape: const StadiumBorder(),
+                    elevation: 0),
+                onPressed: () {
+                  if (isLast) {
+                    Navigator.pushReplacement(context,
+                        MaterialPageRoute(builder: (_) => const HomeScreen()));
+                  } else {
+                    controller.nextPage(
+                        duration: const Duration(milliseconds: 500),
+                        curve: Curves.ease);
+                  }
+                },
+                child: Text(
+                  isLast ? 'Finish' : 'Next',
+                  style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 17,
+                      fontWeight: FontWeight.w500),
+                )),
+
+            /// for adding some space
+            const Spacer(
+              flex: 2,
+            ),
+          ],
+        );
+      },
+    ));
   }
 }
